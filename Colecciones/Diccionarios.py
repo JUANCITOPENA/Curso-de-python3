61f1b32771959fa397179a933f69826c4f9f2f9d
Genarogg <genarrogg@gmail.com>
2020-03-22 23:06:31 -0400

    diccionarios

---
+++ Colecciones/Diccionarios.py
@@ -0,0 +1,9 @@
+diccionarios = {'nombre':"Genaro", 'apellido':"Gonzalez", 'edad':22, 'ciudad':"Maracay"}
+
+nuevo_valor = {'ciudad': "Caracas"}
+
+diccionarios.update(nuevo_valor)
+
+print(diccionarios)
+
+input()
