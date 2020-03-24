40604f4ba2b0b2ab892e350df3665efbbf78706d
Genarogg <genarrogg@gmail.com>
2020-03-23 23:08:56 -0400

    Condiciones

---
+++ FlujosDeInformacion/Condicionales.py
@@ -0,0 +1,11 @@
+a = int(input("Introduse el primer valor: "))
+b = int(input("Introduse el primer valor: "))
+c = int(input("Introduse el primer valor: "))
+
+if a >= b:
+    if a >= c:
+        print("a es el valor mas grade")
+    else:
+        print("a >= b")
+else:
+    print("a < b")
